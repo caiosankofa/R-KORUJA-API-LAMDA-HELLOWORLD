f294e3b0b594bb7c31918c5940d842fc8f8fb5c7
Caio Cesar _ <caioc@Monttech.localdomain>
2023-07-20 15:06:59 -0300

    adicionando pipeline lambda

---
+++ app/func.py
@@ -0,0 +1,11 @@
+def handler(event, context):
+    body = "Hello World New"
+    response = {
+        "statusCode": 200,
+        "statusDescription": "200 OK",
+        "isBase64Encoded": False,
+        "headers": {'Access-Control-Allow-Origin' : '*',"Content-Type": "application/json; charset=utf-8"},
+        "body": body
+        }
+
+    return response
