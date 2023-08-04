b95199ad020144e1089944eca3b2735cc4e14b7c
Caio Cesar _ <caioc@Monttech.localdomain>
2023-08-04 14:44:55 -0300

    nova versao

--- func.py
+++ func.py
@@ -1,11 +1,13 @@
+import json
+
 def handler(event, context):
-    body = "Hello World Eduardo 51"
+    body = "Hello World Eduardo New"
     response = {
         "statusCode": 200,
-        "statusDescription": "200 OK",
-        "isBase64Encoded": False,
-        "headers": {'Access-Control-Allow-Origin' : '*',"Content-Type": "application/json; charset=utf-8"},
-        "body": body
-        }
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({"message": "Holla Caio Monteiro!"})
+    }
 
     return response
